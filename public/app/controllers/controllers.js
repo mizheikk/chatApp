245783b0f0160e582151a999a3e0fffe71c42b58
Mikko Heikkinen <mizheikk@gmail.com>
2015-11-09 21:13:08 +0200

    second commit

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -1,6 +1,7 @@
 var chatAppControllers = angular.module('chatAppControllers', []);
 
 chatAppControllers.controller('setMessageController', function($scope, FirebaseService) {
+
   $scope.addMessage = function() {
     FirebaseService.addMessage("x",$scope.newMessage);
     $scope.newMessage = '';
@@ -10,7 +11,4 @@ chatAppControllers.controller('setMessageController', function($scope, FirebaseS
 chatAppControllers.controller('getMessagesController', function($scope, FirebaseService) {
   var messages = FirebaseService.getMessages();
   $scope.messages = messages;
-});
-
-chatAppControllers.controller('getNewMessageController', function($scope, FirebaseService) {
 });
