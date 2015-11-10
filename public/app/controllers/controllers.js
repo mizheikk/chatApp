4630c70e68e8e870db68a9543cb937733416567f
Mikko Heikkinen <mizheikk@gmail.com>
2015-11-10 11:33:30 +0200

    fourth commit

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -1,22 +1,33 @@
 var chatAppControllers = angular.module('chatAppControllers', []);
 
-chatAppControllers.controller('loginController', function($route, $scope, $location) {
+chatAppControllers.controller('loginController', function($route, $scope, $location, FirebaseService) {
   $scope.addUser = function() {
       var username = $scope.newUsername;
-      $scope.newUsername = 'joo';
-      console.log($location.url());
-      $location.url("chat/"+username);
-      //$location.url("chat.html?user="+username);
+      var promise = FirebaseService.addUser(username);
+      promise.then(function(timestamp) {
+        $location.url("chat/"+username+"/"+timestamp);
+      }, function(reason) {
+        $scope.info='Failed: ' + reason;
+      }, function(update) {
+        $scope.info='Failed: ' + update;
+      });
   }
 });
 
-chatAppControllers.controller('setMessageController', function($routeParams, $scope, FirebaseService) {
+chatAppControllers.controller('setMessageController', function($location, $routeParams, $scope, FirebaseService) {
 
   var username = $routeParams.user;
+  var timestamp = $routeParams.timestamp;
 
   $scope.addMessage = function() {
-    FirebaseService.addMessage(username,$scope.newMessage);
-    $scope.newMessage = '';
+    var promise = FirebaseService.checkTimestamp(username,timestamp);
+    promise.then(function() {
+      FirebaseService.addMessage(username,$scope.newMessage);
+      $scope.newMessage = '';
+    }, function() {
+      $location.url("/");
+      alert("Error! Rotten session..");
+      });
   }
 });
 
--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,6 +1,6 @@
 var chatAppServices = angular.module('chatAppServices', ['firebase']);
 
-chatAppServices.service('FirebaseService', function($firebaseObject, $firebaseArray) {
+chatAppServices.service('FirebaseService', function($firebaseObject, $firebaseArray, $q) {
   var ref = new Firebase('https://luminous-torch-1910.firebaseio.com/');
   var usersRef = ref.child("users");
   var messagesRef = ref.child("messages");
@@ -9,11 +9,36 @@ chatAppServices.service('FirebaseService', function($firebaseObject, $firebaseAr
   var data = $firebaseObject(ref);
 
   this.addUser = function(name) {
+    var deferred = $q.defer();
+
     usersRef.once("value", function(snapshot) {
       if ( snapshot.child(name).exists() == false) {
-        usersRef.child(name).set('');
+        var timestamp = Date.now();
+        usersRef.child(name).set(timestamp, function(error) {
+          if(error) deferred.reject(error);
+          else {
+            deferred.resolve(timestamp);
+          }
+        });
+      }
+      else {
+        deferred.reject("Username is already in use.");
       }
     });
+    return deferred.promise;
+  }
+
+  this.checkTimestamp = function(name,timestamp) {
+    var userRef = new Firebase('https://luminous-torch-1910.firebaseio.com/users/'+name);
+    var deferred = $q.defer();
+
+    userRef.once('value', function(data) {
+      if(data.val() == timestamp)
+        deferred.resolve();
+      else
+        deferred.reject();
+    });
+    return deferred.promise;
   }
 
   this.getMessages = function() {
